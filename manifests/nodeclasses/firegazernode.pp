2ffe281845c5269eea7a4c3e4ef008e647d7d5cf
Bhaskar Maddala <maddalab@gmail.com>
2013-09-26 12:18:24 -0400

    Install the capistrano gem

--- manifests/nodeclasses/firegazernode.pp
+++ manifests/nodeclasses/firegazernode.pp
@@ -1,5 +1,13 @@
 class firegazernode {
   notify {"Kicking off provisioning of firegazer node":}
+
   include java
   include ruby
+
+  # install capistrano for firegazer deployments
+  package { 'capistrano':
+    ensure => present,
+    provider => 'gem',
+    require => Package['rubygems'],
+  }
 }
